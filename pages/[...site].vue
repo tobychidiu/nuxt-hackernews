6a7600fcf83c13df2c8bd79e6dcad6d54f15fb77
Toby Chidi <111127651+tobychidiu@users.noreply.github.com>
2022-08-12 13:48:57 +0100

    moved

--- pages/[...site].vue
+++ pages/[...site].vue
@@ -1,16 +1,17 @@
 <template>
    <div class="news-view">
       <div class="news-list-nav">
-         <a v-if="page > 1" class="page-link" :href="`/${type}?page=${page - 1}`" aria-label="Previous Page">
+         <NuxtLink v-if="page > 1" class="page-link" :to="`/${type}?page=${page - 1}`" aria-label="Previous Page">
             &lt; prev
-         </a>
+         </NuxtLink>
          <span v-else class="page-link disabled" aria-hidden="true">&lt; prev</span>
 
          <span>page {{ page }}</span>
          <!-- sometimes the length isn't 30? -->
-         <a v-if="stories.length >= 28" class="page-link" :href="`/${type}?page=${page + 1}`" aria-label="Next Page">
+         <NuxtLink v-if="stories.length >= 28" class="page-link" :to="`/${type}?page=${page + 1}`"
+            aria-label="Next Page">
             more &gt;
-         </a>
+         </NuxtLink>
          <span v-else class="page-link" aria-hidden="true">more &gt;</span>
       </div>
       <main class="news-list">
@@ -23,10 +24,17 @@
 
 <script setup lang="ts">
 const route = useRoute();
-// console.log(route.params.site)
-const type = route.params.site[0] ?? "top";
-let pageID = ref("1")
-if (route.query.page) { pageID.value = route.query.page.toString() }
-const page = parseInt(pageID.value);
-const stories = await useGetStories(type, page) as [];
+console.log(route.params)
+const type = computed(() => route.params.site[0] ?? "top");
+const page = computed(() => {
+   if (route.query.page) {
+      return parseInt(route.query.page.toString())
+   }
+   return 1
+}
+);
+const { data: stories } = await useAsyncData(async () => await useGetStories(type.value, page.value) as [], {
+   watch: [page, type],
+   initialCache: false
+});
 </script>
